18b2db6ca5f9381c9447c93e085f131ed692e497
jltml <8261330+jltml@users.noreply.github.com>
2021-04-13 00:13:09 -0500

    RubyGems(!!) a global executable! (v0.1.4)

---
+++ lib/vaccine-spotter.rb
@@ -0,0 +1,91 @@
+#################################################################
+# This is basically a wrapper around https://vaccinespotter.org #
+#################################################################
+
+require "open-uri"
+require "json"
+require "net/http"
+require "terminal-notifier"
+require "pastel"
+require "toml"
+require "tty-config"
+require "tty-prompt"
+require "launchy"
+
+pastel = Pastel.new
+config = TTY::Config.new
+prompt = TTY::Prompt.new
+
+config.filename = "vaccine-finder"
+config.extname = ".toml"
+config.append_path "#{Dir.home}/.config"
+
+if !config.exist?
+  puts
+  puts pastel.bold "Hi, welcome to the unofficial vaccinespotter.org CLI notifier thing!"
+  puts "It looks like you don't have a configuration file yet. Would you like to create one?"
+  create_config = prompt.yes? "Create `~/.config/vaccine-finder.toml`?"
+  if create_config
+    set_state = prompt.ask("What state would you like to monitor? (Two-letter postal code, please)", required: true)
+    config.set(:state, value: set_state)
+    puts "Ok, time to set zip codes…"
+    if prompt.yes? "freemaptools.com has a really helpful tool for getting the zip codes around a radius. Would you like to open it?"
+      Launchy.open "https://www.freemaptools.com/find-zip-codes-inside-radius.htm"
+    end
+    set_zips = prompt.ask("What zip codes would you like to track? You can paste here from freemaptools.com, if you used it (comma-separated list of integers, please)", convert: :int_list, required: true)
+    config.set(:zips, value: set_zips)
+    set_refresh_rate = prompt.ask("How often should the script check for updates? (seconds)", convert: :int, required: true)
+    config.set(:refresh_rate, value: set_refresh_rate)
+    puts "Perfect; writing to `~/.config/vaccine-finder.toml`…"
+    config.write
+  end
+end
+
+zips = config.read["zips"]
+state = config.read["state"]
+refresh_rate = config.read["refresh_rate"].to_i
+
+puts
+print pastel.bold "vaccinespotter.org CLI notifier thing"
+print " v0.1.4"
+puts pastel.dim ", by Jack MapelLentz (jltml.me)"
+puts pastel.dim "This script uses the wonderful vaccinespotter.org, by Nick Muerdter (github.com/GUI)"
+puts "Checking #{pastel.underline zips.length} zip codes in #{pastel.underline state} every #{pastel.underline refresh_rate} seconds"
+
+excluded = Array.new
+
+loop do
+
+  number_excluded = excluded.length
+
+  json = Net::HTTP.get(URI("https://www.vaccinespotter.org/api/v0/states/#{state}.json"))
+  parsed_json = JSON.parse(json)
+
+  puts
+
+  if (Time.now.strftime("%M").to_i%5 == 0) && (Time.now.strftime("%S").to_i < refresh_rate)
+    excluded.clear
+    puts "List of excluded/checked stores cleared"
+  end
+
+  parsed_json["features"].each_index do |i|
+    current = parsed_json["features"][i]["properties"]
+    if (current["appointments_available"] == true) and (zips.include?(current["postal_code"].to_i) == true) and (!excluded.include? current["id"].to_i) and (current["appointment_vaccine_types"]["pfizer"] == true or current["appointment_vaccine_types"]["unknown"] == true)
+      puts "- #{pastel.green.bold current["city"]} #{pastel.green.bold current["provider_brand_name"]}: #{current["url"]}"
+      puts " - #{current["appointment_vaccine_types"]} as of #{Time.parse(current["appointments_last_fetched"]).localtime.strftime("%H:%M:%S")}"
+      TerminalNotifier.notify("#{current["provider_brand_name"]} - #{current["city"]}", :title => "vaccine-spotter", :open => "#{current["url"]}")
+      excluded.append current["id"].to_i
+    end
+  end
+
+  print pastel.dim "Checked at #{Time.now.strftime("%H:%M:%S")} on #{Time.now.strftime("%Y-%m-%d")}"
+  if number_excluded == 1
+    print pastel.dim " (#{number_excluded} checked location excluded)"
+  elsif number_excluded != 0
+    print pastel.dim " (#{number_excluded} checked locations excluded)"
+  end
+  puts
+
+  sleep refresh_rate
+
+end
---
+++ vaccine-finder.gemspec
@@ -0,0 +1,27 @@
+Gem::Specification.new do |s|
+  s.name        = 'vaccine-finder'
+  s.version     = '0.1.4'
+  s.summary     = "Get notified of vaccine availability"
+  s.description = "This gem will notify you when COVID-19 vaccine appointments are available matching certain criteria (a list of zip codes, type of vaccine, etc). It currently pretty much just wraps the very beta API from the absolutely wonderful vaccinespotter.org, though I hope to add my own website scraping soon too so as to improve response times."
+  s.authors     = ["Jack MapelLentz"]
+  s.files       = ["lib/vaccine-spotter.rb"]
+  s.homepage    =
+    'https://github.com/jltml/covid-vaccine-finders'
+  s.license       = 'MIT'
+  s.executables << 'vaccine-finder'
+  s.add_runtime_dependency "curb", "~> 0.9.11"
+  s.add_runtime_dependency "tty-progressbar", "~> 0.18.2"
+  # s.add_runtime_dependency "tty-option", "~> 0.1.0"
+  s.add_runtime_dependency "tty-logger", "~> 0.6.0"
+  s.add_runtime_dependency "tty-prompt", "~> 0.23.0"
+  s.add_runtime_dependency "tty-spinner", "~> 0.9.3"
+  s.add_runtime_dependency 'tty-config', '~> 0.4.0'
+  s.add_runtime_dependency 'toml', '~> 0.2.0'
+  # s.add_runtime_dependency "mechanize", "~> 2.7"
+  s.add_runtime_dependency "watir", "~> 6.19"
+  s.add_runtime_dependency 'webdrivers', '~> 4.0'
+  s.add_runtime_dependency "nokogiri", "~> 1.11"
+  s.add_runtime_dependency 'pastel', '~> 0.8.0'
+  s.add_runtime_dependency "terminal-notifier", "~> 2.0"
+  s.add_runtime_dependency 'launchy', '~> 2.5'
+end
